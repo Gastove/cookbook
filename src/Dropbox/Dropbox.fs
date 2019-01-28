023a0ccd41ff606c564a905ce08b05eb7369b46f
Ross Donaldson <gastove@gmail.com>
2019-01-27 19:15:49 -0800

    Specify netcoreapp2.2

--- src/Dropbox/Dropbox.fs
+++ src/Dropbox/Dropbox.fs
@@ -44,24 +44,3 @@ module Files =
         |> IO.Path.Combine
         |> dbxClient.Files.DownloadAsync
         |> Async.AwaitTask
-
-    [<EntryPoint>]
-    let main argv =
-        let folder = "/the_range/test/blog/"
-        let fileName = "post-one.html"
-
-        match Auth.createDbxClient() with
-        | Some cli ->
-            let file =
-                loadFilAsync folder fileName cli
-                |> Async.RunSynchronously
-
-            let reader =
-                file.GetContentAsStreamAsync()
-                |> Async.AwaitTask
-                |> Async.RunSynchronously
-                |> fun io -> new IO.StreamReader(io)
-
-            printfn "Got:\n%s" (reader.ReadToEnd())
-        | None -> printfn "Something went wrong :/"
-        0
