29ca014ffb247c2c755abff6bf94246ae5057373
Ross Donaldson <gastove@gmail.com>
2019-02-18 21:54:01 -0800

    Load blog posts!

--- src/Cookbook/Site.fs
+++ src/Cookbook/Site.fs
@@ -7,12 +7,13 @@ open WebSharper.UI.Server
 
 type EndPoint =
     | [<EndPoint "/">] Home
-    // | [<EndPoint "/blog">] Blog of string
+    | [<EndPoint "/blog">] Blog of string
 
 module Templating =
     open WebSharper.UI.Html
 
     type MainTemplate = Templating.Template<"templates/main.html">
+    type PostTemplate = Templating.Template<"templates/post.html">
 
     // Compute a menubar where the menu item for the given endpoint is active
     let MenuBar (ctx: Context<EndPoint>) endpoint : Doc list =
@@ -28,12 +29,26 @@ module Templating =
     let Main (blogPosts: BlogPost array) =
         let postSummaries =
             blogPosts
-            |> Array.map(fun (post : BlogPost) -> MainTemplate.PostSummary().PostTitle(post.Title).Summary(post.Meta.Summary).Doc())
+            |> Array.map(fun (post : BlogPost) ->
+                         MainTemplate.PostSummary()
+                             .PostTitle(post.Title)
+                             .PostSlug(post.Meta.Slug)
+                             .Summary(post.Meta.Summary)
+                             .Doc()
+                         )
 
         Content.Page(
             MainTemplate()
                 .Title("A Blog?!")
                 .PostSumaries(postSummaries)
+                .Doc()
+            )
+
+    let Post (post: BlogPost) =
+        Content.Page(
+            PostTemplate()
+                .Title(post.Title)
+                .Body(post.Body)
                 .Doc()
             )
 
@@ -47,10 +62,19 @@ module Site =
             Templating.Main posts
         | None -> Templating.Main [||]
 
+
+    let BlogPost slug =
+        let postFile = slug + ".html"
+        match Dropbox.Auth.createDbxClient() with
+        | Some client ->
+            let post = Blog.loadPost "/the_range/test/blog" postFile client
+            Templating.Post post
+        | None -> Templating.Main [||]
+
     [<Website>]
     let Main =
         Application.MultiPage (fun ctx endpoint ->
             match endpoint with
             | EndPoint.Home -> HomePage()
-            // | EndPoint.Blog (slug) -> BlogPost ctx slug
+            | EndPoint.Blog (slug) -> BlogPost slug
         )
