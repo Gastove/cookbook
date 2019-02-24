fb6bf41c6f36f12ffed72bd9914c18fb6ee41e57
Ross Donaldson <gastove@gmail.com>
2019-02-23 19:08:09 -0800

    Move base XML in to code
    Turns out, there isn't really a tidy way to keep these as files. Neat? As code
    it is.

--- src/Xml/Xml.fs
+++ src/Xml/Xml.fs
@@ -34,13 +34,6 @@ module Xml =
     let loadPost path =
         IO.File.OpenRead(path)
 
-    let loadXmlDoc path =
-        let file = IO.File.OpenRead(path)
-        let reader = new IO.StreamReader(file)
-        let doc = Xml.XmlDocument()
-        doc.Load(reader)
-        doc
-
     let separatePostAndMeta (doc : string) =
         let startTag = "<POST-META>"
         let endTag = "</POST-META>"
@@ -81,11 +74,48 @@ module Feed =
 
     let hostName = "http://35.185.199.14/"
     let blogPath = hostName + "blog/"
+    let blogTitle = "blog.gastove.com"
 
     module Atom =
 
-        let loadBase() =
-            Xml.loadXmlDoc("atom.xml")
+        let createBase() =
+            let doc = System.Xml.XmlDocument()
+            let feed = doc.CreateElement("feed")
+            let feedNs = doc.CreateAttribute("xmlns")
+            feedNs.Value <- "http://www.w3.org/2005/Atom"
+            feed.Attributes.Append(feedNs) |> ignore
+
+            doc.AppendChild(feed) |> ignore
+
+            let title = doc.CreateElement("title")
+            title.InnerText <- blogTitle
+            doc.DocumentElement.AppendChild(title) |> ignore
+
+            let link = doc.CreateElement("link")
+            let linkHref = doc.CreateAttribute("href")
+            let linkRel = doc.CreateAttribute("rel")
+            linkHref.Value <- "http://35.185.199.14/feed/atom"
+            linkRel.Value <- "self"
+            link.Attributes.Append(linkHref) |> ignore
+            link.Attributes.Append(linkRel) |> ignore
+            doc.DocumentElement.AppendChild(link) |> ignore
+
+            let updated = doc.CreateElement("updated")
+            updated.InnerText <- DateTime.Now.ToString("s")
+            doc.DocumentElement.AppendChild(updated) |> ignore
+
+            let author = doc.CreateElement("author")
+            let authorName = doc.CreateElement("name")
+            authorName.InnerText <- "Ross Donaldson"
+            author.AppendChild(authorName) |> ignore
+            doc.DocumentElement.AppendChild(author) |> ignore
+
+            let id = doc.CreateElement("id")
+            id.InnerText <- hostName
+            doc.DocumentElement.AppendChild(id) |> ignore
+
+            doc
+
 
         let postToItem (doc : Xml.XmlDocument) (post : BlogPost) =
             let entry = doc.CreateElement("entry")
@@ -125,7 +155,29 @@ module Feed =
     module RSS =
 
         let loadBase() =
-            Xml.loadXmlDoc("rss.xml")
+            let doc = System.Xml.XmlDocument()
+            let rss = doc.CreateElement("rss")
+            let rssVersion = doc.CreateAttribute("version")
+            rssVersion.Value <- "2.0"
+            rss.Attributes.Append(rssVersion) |> ignore
+            doc.AppendChild(rss) |> ignore
+
+            let channel = doc.CreateElement("channel")
+
+            let title = doc.CreateElement("title")
+            title.InnerText <- blogTitle
+            let link = doc.CreateElement("link")
+            link.InnerText <- hostName
+            let description = doc.CreateElement("description")
+            description.InnerText <- "The Blog of Ross Donaldson"
+
+            channel.AppendChild(title) |> ignore
+            channel.AppendChild(description) |> ignore
+            channel.AppendChild(link) |> ignore
+
+            doc.DocumentElement.AppendChild(channel) |> ignore
+
+            doc
 
         let postToItem (doc : Xml.XmlDocument) (post : BlogPost) =
             let item = doc.CreateElement("item")
@@ -148,12 +200,12 @@ module Feed =
 
         let updateFeedWith (feed : Xml.XmlDocument) (post : BlogPost) =
             let item = postToItem feed post
-            feed.DocumentElement.AppendChild(item) |> ignore
+            feed.DocumentElement.FirstChild.AppendChild(item) |> ignore
             feed
 
 
     let formatFeedAtom (posts : BlogPost list) =
-        let feed = Atom.loadBase()
+        let feed = Atom.createBase()
         posts
         |> List.sortBy (fun post -> post.Meta.PublicationDate)
         |> List.rev
