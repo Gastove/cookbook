a555801d47f43802272531d71660729e70370646
Ross Donaldson <gastove@gmail.com>
2019-10-05 14:00:15 -0700

    Regenerate JS

--- src/Cookbook/wwwroot/Scripts/WebSharper/Cookbook.js
+++ src/Cookbook/wwwroot/Scripts/WebSharper/Cookbook.js
@@ -23,7 +23,7 @@
   vReversed=View.MapAsync(function(a)
   {
    var b;
-   return a!=null&&a.$==1?(new AjaxRemotingProvider.New()).Async("Cookbook:Cookbook.Server.DoSomething:-1393111868",[a.$0]):(b=null,Concurrency.Delay(function()
+   return a!=null&&a.$==1?(new AjaxRemotingProvider.New()).Async("Cookbook:Cookbook.Server.DoSomething:899449445",[a.$0]):(b=null,Concurrency.Delay(function()
    {
     return Concurrency.Return("");
    }));
