7344eb99cbf32de28db11dae293e54bfb04a328f
Ross Donaldson <gastove@gmail.com>
2021-01-05 10:21:44 -0800

    Strip out all code use of WebSharper, replace with tinker-toy Giraffe app
    This commit removes WebSharper from all code. Everything that needs to be
    commented out is, and a very small amount of Giraffe is implemented in its
    place. This compiles, runs, generally Does the Thing, for a very small,
    simplistic notion of "thing".

--- src/Cookbook/Site.fs
+++ src/Cookbook/Site.fs
@@ -1,145 +1,151 @@
 namespace Cookbook
 
-open WebSharper
-open WebSharper.Sitelets
-open WebSharper.UI
-open WebSharper.UI.Server
+open Giraffe
 
-type EndPoint =
-    | [<EndPoint"/">] Home
-    | [<EndPoint"/blog">] Blog of string
-    | [<EndPoint"/feed/atom">] Feed
+// type EndPoint =
+//     | [<EndPoint"/">] Home
+//     | [<EndPoint"/blog">] Blog of string
+//     | [<EndPoint"/feed/atom">] Feed
 
 module Templating =
-    open WebSharper.UI.Html
+    open Giraffe.ViewEngine
 
-    open Serilog
+    let index () =
+        html [] [
+            head [] [
+                title [] [ encodedText "BLOG WOO YEAH" ]
+                link [ _rel "stylesheet"
+                       _type "text/css"
+                       _href "css/screen.css" ]
+            ]
+            body [] [ encodedText "YEAH BLOG" ]
+        ]
 
-    type MainTemplate = Templating.Template<"templates/main.html">
-
-    type HeaderTemplate = Templating.Template<"templates/header.html">
-
-    type PostTemplate = Templating.Template<"templates/post.html">
-
-    let logger = LoggerConfiguration().WriteTo.Console().CreateLogger()
+module Handlers =
+    // type MainTemplate = Templating.Template<"templates/main.html">
+    // type HeaderTemplate = Templating.Template<"templates/header.html">
+    // type PostTemplate = Templating.Template<"templates/post.html">
+    let indexHandler () =
+        let view = Templating.index ()
+        htmlView view
 
     // One hour in seconds
     let MaxAge = 3600
 
-    let computeHeaders() =
-        [ Http.Header.Custom "Cache-Control"
-              (sprintf "public,max-age=%i" MaxAge) ]
+// TODO[gastove|2021-01-05] I definitely need cache control headers; gonna
+// have to see what I get for free from modern ASPNET.
+//
+// let computeHeaders() =
+//     [ Http.Header.Custom "Cache-Control"
+//           (sprintf "public,max-age=%i" MaxAge) ]
 
-    let WithCacheHeaders content =
-        let headers = computeHeaders()
-        content |> Content.WithHeaders headers
 
-    // Compute a menubar where the menu item for the given endpoint is active
-    let MenuBar (ctx : Context<EndPoint>) endpoint : Doc list =
-        let (=>) txt act =
-            li [ if endpoint = act then yield attr.``class`` "active" ]
-                [ a [ attr.href (ctx.Link act) ] [ text txt ] ]
-        [ "Home" => EndPoint.Home ]
+// Compute a menubar where the menu item for the given endpoint is active
+// let MenuBar (ctx : Context<EndPoint>) endpoint : Doc list =
+//     let (=>) txt act =
+//         li [ if endpoint = act then yield attr.``class`` "active" ]
+//             [ a [ attr.href (ctx.Link act) ] [ text txt ] ]
+//     [ "Home" => EndPoint.Home ]
 
-    let PageHeader(title : string) =
-        HeaderTemplate()
-            .Title(title)
-            .Doc()
+// let PageHeader(title : string) =
+//     HeaderTemplate()
+//         .Title(title)
+//         .Doc()
 
-    // Footer requires no templating, so we just compute it.
-    let Footer(extra : Doc list) =
-        let sep = text " | "
+// // Footer requires no templating, so we just compute it.
+// let Footer(extra : Doc list) =
+//     let sep = text " | "
 
-        let baseFooter =
-            [ a [ attr.href "/" ] [ text "Home" ]
-              sep
-              a [ attr.href "https://gastove.com" ] [ text "gastove.com" ]
-              sep
-              a [ attr.href "https://gitlab.com/gastove" ] [ text "Gitlab" ]
-              sep
-              a [ attr.href "https://github.com/Gastove" ] [ text "Github" ]
-              sep
-              a [ attr.href "/feed/atom" ] [ text "Atom" ]
-              br [] []
-              br [] []
-              text "© Ross Donaldson" ]
-        footer [] (List.append baseFooter extra)
+//     let baseFooter =
+//         [ a [ attr.href "/" ] [ text "Home" ]
+//           sep
+//           a [ attr.href "https://gastove.com" ] [ text "gastove.com" ]
+//           sep
+//           a [ attr.href "https://gitlab.com/gastove" ] [ text "Gitlab" ]
+//           sep
+//           a [ attr.href "https://github.com/Gastove" ] [ text "Github" ]
+//           sep
+//           a [ attr.href "/feed/atom" ] [ text "Atom" ]
+//           br [] []
+//           br [] []
+//           text "© Ross Donaldson" ]
+//     footer [] (List.append baseFooter extra)
 
-    let Main(blogPosts : BlogPost array) =
-        let title = "blog.gastove.com"
+//     let Main(blogPosts : BlogPost array) =
+//         let title = "blog.gastove.com"
 
-        let postSummaries =
-            blogPosts
-            |> Array.sortBy (fun post -> post.Meta.PublicationDate)
-            |> Array.rev
-            |> Array.map
-                (fun (post : BlogPost) ->
-                MainTemplate.PostSummary().PostTitle(post.Title)
-                            .PostSlug(post.Meta.Slug)
-                            .Summary(post.Meta.Summary).Doc())
-        Content.Page(MainTemplate()
-                .Header(PageHeader title)
-                .Title(title)
-                .PostSumaries(postSummaries)
-                .Footer(Footer List.Empty)
-                .Doc()) |> WithCacheHeaders
+//         let postSummaries =
+//             blogPosts
+//             |> Array.sortBy (fun post -> post.Meta.PublicationDate)
+//             |> Array.rev
+//             |> Array.map
+//                 (fun (post : BlogPost) ->
+//                 MainTemplate.PostSummary().PostTitle(post.Title)
+//                             .PostSlug(post.Meta.Slug)
+//                             .Summary(post.Meta.Summary).Doc())
+//         Content.Page(MainTemplate()
+//                 .Header(PageHeader title)
+//                 .Title(title)
+//                 .PostSumaries(postSummaries)
+//                 .Footer(Footer List.Empty)
+//                 .Doc()) |> WithCacheHeaders
 
-    let Post(post : BlogPost) =
-        let scripts = [ script [ attr.src "/js/prism.js" ] [] ]
-        Content.Page(PostTemplate()
-                .Header(PageHeader post.Title)
-                .PostTitle(post.Title)
-                .Body(post.Body)
-                .PostDate(post.Meta.PublicationDate.ToLongDateString())
-                .Tags(post.Meta.Tags)
-                .Footer(Footer scripts)
-                .Doc()) |> WithCacheHeaders
+//     let Post(post : BlogPost) =
+//         let scripts = [ script [ attr.src "/js/prism.js" ] [] ]
+//         Content.Page(PostTemplate()
+//                 .Header(PageHeader post.Title)
+//                 .PostTitle(post.Title)
+//                 .Body(post.Body)
+//                 .PostDate(post.Meta.PublicationDate.ToLongDateString())
+//                 .Tags(post.Meta.Tags)
+//                 .Footer(Footer scripts)
+//                 .Doc()) |> WithCacheHeaders
 
-    let Feed(doc : System.Xml.XmlDocument) =
-        let feedHeaders =
-            [ [ Http.Header.Custom "Content-Type" "application/atom+xml" ]
-              computeHeaders() ]
-            |> List.concat
-        Content.Custom(Status = Http.Status.Ok, Headers = feedHeaders,
-                       WriteBody = fun stream ->
-                           use w = new System.IO.StreamWriter(stream)
-                           doc.Save(w))
+//     let Feed(doc : System.Xml.XmlDocument) =
+//         let feedHeaders =
+//             [ [ Http.Header.Custom "Content-Type" "application/atom+xml" ]
+//               computeHeaders() ]
+//             |> List.concat
+//         Content.Custom(Status = Http.Status.Ok, Headers = feedHeaders,
+//                        WriteBody = fun stream ->
+//                            use w = new System.IO.StreamWriter(stream)
+//                            doc.Save(w))
 
-module Site =
+// module Site =
 
-    let HomePage(cfg : Configuration) =
-        match Dropbox.Auth.createDbxClient() with
-        | Some client ->
-            use client = client
-            let posts = Blog.loadAllPosts cfg.BlogDir client
-            Templating.Main posts
-        | None -> Templating.Main [||]
+//     let HomePage(cfg : Configuration) =
+//         match Dropbox.Auth.createDbxClient() with
+//         | Some client ->
+//             use client = client
+//             let posts = Blog.loadAllPosts cfg.BlogDir client
+//             Templating.Main posts
+//         | None -> Templating.Main [||]
 
-    let BlogPost (cfg : Configuration) slug =
-        let postFile = slug + ".html"
-        match Dropbox.Auth.createDbxClient() with
-        | Some client ->
-            use client = client
-            let post = Blog.loadPost cfg.BlogDir postFile client
-            Templating.Post post
-        | None -> Templating.Main [||]
+//     let BlogPost (cfg : Configuration) slug =
+//         let postFile = slug + ".html"
+//         match Dropbox.Auth.createDbxClient() with
+//         | Some client ->
+//             use client = client
+//             let post = Blog.loadPost cfg.BlogDir postFile client
+//             Templating.Post post
+//         | None -> Templating.Main [||]
 
-    let PublishFeed(cfg : Configuration) =
-        match Dropbox.Auth.createDbxClient() with
-        | Some client ->
-            use client = client
-            let posts = Blog.loadAllPosts cfg.BlogDir client |> Array.toList
-            let feed = Feed.formatFeed posts
-            Templating.Feed feed
-        | None -> WebSharper.Sitelets.Content.NotFound
+//     let PublishFeed(cfg : Configuration) =
+//         match Dropbox.Auth.createDbxClient() with
+//         | Some client ->
+//             use client = client
+//             let posts = Blog.loadAllPosts cfg.BlogDir client |> Array.toList
+//             let feed = Feed.formatFeed posts
+//             Templating.Feed feed
+//         | None -> WebSharper.Sitelets.Content.NotFound
 
-    [<Website>]
-    let Main =
-        let logger = Logging.ConfigureLogging()
-        let cfg = Config.loadConfig()
-        Async.Start <| Static.Sync.runSync cfg logger
-        Application.MultiPage(fun _ctx endpoint ->
-            match endpoint with
-            | EndPoint.Home -> HomePage cfg
-            | EndPoint.Blog(slug) -> BlogPost cfg slug
-            | EndPoint.Feed -> PublishFeed cfg)
+//     [<Website>]
+//     let Main =
+//         let logger = Logging.ConfigureLogging()
+//         let cfg = Config.loadConfig()
+//         Async.Start <| Static.Sync.runSync cfg logger
+//         Application.MultiPage(fun _ctx endpoint ->
+//             match endpoint with
+//             | EndPoint.Home -> HomePage cfg
+//             | EndPoint.Blog(slug) -> BlogPost cfg slug
+//             | EndPoint.Feed -> PublishFeed cfg)
--- src/Cookbook/Startup.fs
+++ src/Cookbook/Startup.fs
@@ -1,49 +1,77 @@
 namespace Cookbook
 
-open Serilog
-open Serilog.AspNetCore
 open System
-open Microsoft.AspNetCore
+open System.IO
+
 open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Cors.Infrastructure
 open Microsoft.AspNetCore.Hosting
-open Microsoft.AspNetCore.Http
-open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.DependencyInjection
-open Microsoft.Extensions.Logging
-open WebSharper.AspNetCore
 
-type Startup() =
+open Giraffe
 
-    member this.ConfigureServices(services: IServiceCollection) =
-        services.AddSitelet(Site.Main)
-            .AddAuthentication("WebSharper")
-            .AddCookie("WebSharper", fun options -> ())
+
+module Server =
+    open Giraffe
+    open Microsoft.Extensions.Logging
+
+    let webApp =
+        choose [ GET >=> choose [ route "/" >=> Handlers.indexHandler () ]
+                 setStatusCode 404 >=> text "Not Found" ]
+
+    let errorHandler (ex: Exception) (logger: ILogger) =
+        logger.LogError(ex, "An unhandled exception has occurred while executing the request.")
+
+        clearResponse
+        >=> setStatusCode 500
+        >=> text ex.Message
+
+    let configureCors (builder: CorsPolicyBuilder) =
+        builder
+            .WithOrigins("http://localhost:5000", "https://localhost:5001")
+            .AllowAnyMethod()
+            .AllowAnyHeader()
         |> ignore
 
-    member this.Configure(app: IApplicationBuilder, env: IWebHostEnvironment) =
-        if (env.EnvironmentName = "Development")
-        then
-            app.UseDeveloperExceptionPage() |> ignore
+    let configureServices (services : IServiceCollection) =
+        services.AddCors()    |> ignore
+        services.AddGiraffe() |> ignore
 
-        app.UseAuthentication()
+    let configureApp (app: IApplicationBuilder) =
+        let env =
+            app.ApplicationServices.GetService<IWebHostEnvironment>()
+
+        (match env.IsDevelopment() with
+         | true -> app.UseDeveloperExceptionPage()
+         | false ->
+             app
+                 .UseGiraffeErrorHandler(errorHandler)
+                 .UseHttpsRedirection())
+            .UseCors(configureCors)
             .UseStaticFiles()
-            .UseWebSharper()
-            .Run(fun context ->
-                context.Response.StatusCode <- 404
-                context.Response.WriteAsync("Page not found"))
+            .UseGiraffe(webApp)
 
-module Program =
-
-    let BuildWebHost args =
-        WebHost
-            .CreateDefaultBuilder(args)
-            .UseStartup<Startup>()
-            .ConfigureKestrel(fun opts -> opts.AllowSynchronousIO <- true)
-            .UseSerilog()
-            .Build()
+module Main =
+    open Serilog
 
     [<EntryPoint>]
     let main args =
         Log.Logger <- Logging.ConfigureLogging()
-        BuildWebHost(args).Run()
+        let contentRoot = Directory.GetCurrentDirectory()
+        let webRoot = Path.Combine(contentRoot, "wwwroot")
+
+        Host
+            .CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(fun webHostBuilder ->
+                webHostBuilder
+                    .UseContentRoot(contentRoot)
+                    .UseWebRoot(webRoot)
+                    .Configure(Action<IApplicationBuilder> Server.configureApp)
+                    .ConfigureServices(Server.configureServices)
+                    .UseSerilog()
+                |> ignore)
+            .Build()
+            .Run()
+
         0
