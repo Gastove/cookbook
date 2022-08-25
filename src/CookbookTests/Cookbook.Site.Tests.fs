9f50d83abcc7a6b57fbeb5863d6d91fbb8be5a02
Ross Donaldson <gastove@gmail.com>
2022-08-25 12:29:57 -0700

    Get the tests passing, add a new test

--- src/CookbookTests/Cookbook.Site.Tests.fs
+++ src/CookbookTests/Cookbook.Site.Tests.fs
@@ -25,10 +25,35 @@ let templatingTests =
                   div [] [
                       str "Tags: "
                       a [ _href "/blog/filter/tag/tag" ] [
-                          str "Tag "
+                          str "Tag"
                       ]
                   ]
 
               let got = input |> Templating.splitAndFormatTags
 
-              Expect.equal got expected "We should be able to HTML-ize a single tag" ]
+              Expect.equal got expected "We should be able to HTML-ize a single tag"
+
+          testCase "Test Templating.splitAndFormatTags with multiple tags"
+          <| fun _ ->
+              let input = ":tag1:tag2:tag3:"
+              let sep = str " | "
+              let expected =
+                  div [] [
+                      str "Tags: "
+                      a [ _href "/blog/filter/tag/tag1" ] [
+                          str "Tag1"
+                      ]
+                      sep
+                      a [ _href "/blog/filter/tag/tag2" ] [
+                          str "Tag2"
+                      ]
+                      sep
+                      a [ _href "/blog/filter/tag/tag3" ] [
+                          str "Tag3"
+                      ]
+                  ]
+
+              let got = input |> Templating.splitAndFormatTags
+
+              Expect.equal got expected "We should be able to HTML-ize a single tag"
+          ]
