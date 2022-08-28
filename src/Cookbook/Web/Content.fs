13851ed3831b07b8ad0ca277b76d9e696f7f3c21
Ross Donaldson <gastove@gmail.com>
2022-08-28 07:35:37 -0700

    Implement tag manifest loading, protect XML parsing
    We want to parse tag manifests from org-blorg, which will be authoritative lists
    of all the tags used in a project. This is a lovely idea, and also, it turned up
    some _stuff_.
    
    First, very practically, we add Thoth for JSON parsing. I could do this a
    zillion ways, but Thoth is nice. I don't even need a wrapper type for Tags, so
    far. Tag manifest parsing can then be wired up and percolated through, which requires
    Result handling. Not bad.
    
    But: while doing this, I learned that FSharp.Data's XML parser will *detonate*
    if you, say, accidentally feed it JSON -- and the errors are horrible and very,
    very opaque. So, I wrapped that in a Result as well, which requires a bit more
    percolating through the stack. Should be... I dunno, at least a lot less
    dramatic now.
    
    This is all set, once tags are fixed on my blog!

--- src/Cookbook/Web/Content.fs
+++ src/Cookbook/Web/Content.fs
@@ -6,8 +6,10 @@ module Content =
 
     open Giraffe.ViewEngine.HtmlElements
     open Serilog
+    open Thoth.Json.Net
 
     open Cookbook
+    open Cookbook.Common
 
     type Content =
         | HTMLView of XmlNode
@@ -55,8 +57,8 @@ module Content =
 
         member this.RenderForHTML() =
             match this with
-                | Id -> "all"
-                | Tag s -> s
+            | Id -> "all"
+            | Tag s -> s
 
     let filterBlogPosts (filter: BlogFilter) (posts: BlogPost array) =
         let filterFn =
@@ -66,49 +68,62 @@ module Content =
 
         posts |> Array.filter filterFn
 
-    let filteredBlogIndexContent
-        (filter: BlogFilter)
-        (client: IStorageClient)
-        (cfg: CookbookConfig)
-        =
+    let loadTags (client: IStorageClient) (cfg: CookbookConfig) =
+        task {
+            let! tagsJson = client.Get cfg.StaticAssetsBucket $"{cfg.BlogDir}/tags-manifest.json"
+            let tagsManifest = tagsJson |> Decode.fromString (Decode.field "tags" (Decode.list Decode.string))
+            return tagsManifest
+        }
+
+    let filteredBlogIndexContent (filter: BlogFilter) (client: IStorageClient) (cfg: CookbookConfig) =
         task {
             let blogTitle = "gastove.com/blog"
 
-            let! posts = Blog.loadAllPosts cfg.StaticAssetsBucket cfg.BlogDir client
+            let! postResult = Blog.loadAllPosts cfg.StaticAssetsBucket cfg.BlogDir client
 
             Log.Information("Filtering by {Filter}", filter)
 
-            let allTags = ["live"; "food"]
+            let! tagsManifest = loadTags client cfg
 
-            let summaries =
-                posts
-                |> filterBlogPosts filter
-                |> Array.sortByDescending Blog.projectPublicationDate
-                |> Templating.postSummaries (filter.RenderForHTML() |> Some) allTags
+            match (postResult |> Result.gather, tagsManifest)  with
+            | Ok posts, Ok allTags ->
+                let summaries =
+                    posts
+                    |> List.toArray
+                    |> filterBlogPosts filter
+                    |> Array.sortByDescending Blog.projectPublicationDate
+                    |> Templating.postSummaries (filter.RenderForHTML() |> Some) allTags
 
-            let view =
-                Templating.page blogTitle List.empty summaries
+                let view =
+                    Templating.page blogTitle List.empty summaries
 
-            return view |> HTMLView |> Ok
+                return view |> HTMLView |> Ok
+
+            | (_, Error e) -> return (500, e) |> Error
+            | (Error e, _) -> return (500, e.Message) |> Error
         }
-
 
     let blogIndexContent (client: IStorageClient) (cfg: CookbookConfig) =
         task {
             let blogTitle = "gastove.com/blog"
 
-            let! posts = Blog.loadAllPosts cfg.StaticAssetsBucket cfg.BlogDir client
+            let! postResult = Blog.loadAllPosts cfg.StaticAssetsBucket cfg.BlogDir client
 
-            let allTags = ["live"; "food"]
+            let! tagsManifest = loadTags client cfg
+            match (postResult |> Result.gather, tagsManifest) with
+            | Ok posts, Ok allTags ->
+                let summaries =
+                    posts
+                    |> List.toArray
+                    |> Array.sortByDescending Blog.projectPublicationDate
+                    |> Templating.postSummaries None allTags
 
-            let summaries =
-                posts
-                |> Array.sortByDescending Blog.projectPublicationDate
-                |> Templating.postSummaries None allTags
+                let view =
+                    Templating.page blogTitle List.empty summaries
 
-            let view = Templating.page blogTitle List.empty summaries
-
-            return view |> HTMLView |> Ok
+                return view |> HTMLView |> Ok
+            | (_, Error e) -> return (500, e) |> Error
+            | (Error e, _) -> return (500, e.Message) |> Error
         }
 
     let blogPostContent (slug: string) =
@@ -117,24 +132,31 @@ module Content =
                 let blogTitle = "gastove.com/blog"
                 let postFile = $"{slug}.html"
 
-                let! post = Blog.loadPost cfg.StaticAssetsBucket $"{cfg.BlogDir}/{postFile}" client
+                let! postResult = Blog.loadPost cfg.StaticAssetsBucket $"{cfg.BlogDir}/{postFile}" client
 
-                let postPage = post |> Templating.postView
+                match postResult with
+                | Ok post ->
+                    let postPage = post |> Templating.postView
 
-                let view =
-                    Templating.page blogTitle Templating.postFooterExtras postPage
+                    let view =
+                        Templating.page blogTitle Templating.postFooterExtras postPage
 
-                return view |> HTMLView |> Ok
+                    return view |> HTMLView |> Ok
+                | Error e -> return (500, e.Message) |> Error
             }
 
     let feedContent client (cfg: CookbookConfig) =
         task {
-            let! posts = Blog.loadAllPosts cfg.StaticAssetsBucket cfg.BlogDir client
+            let! postResult = Blog.loadAllPosts cfg.StaticAssetsBucket cfg.BlogDir client
 
-            let feed = Feed.formatFeed <| List.ofArray posts
+            match (postResult |> Result.gather) with
+            | Ok posts ->
 
-            return
-                ("application/atom+xml", feed.OuterXml)
-                |> StringData
-                |> Ok
+                let feed = Feed.formatFeed posts
+
+                return
+                    ("application/atom+xml", feed.OuterXml)
+                    |> StringData
+                    |> Ok
+            | Error e -> return (500, e.Message) |> Error
         }
